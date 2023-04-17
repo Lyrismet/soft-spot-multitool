d6c95f89e5e40bb2706e4aa2c263b4259d8b613c
Lyrismet <ana0203tir@gmail.com>
2023-04-17 22:53:30 +0700

    Modal windows works right

--- src/Components/Calculator/Calculator.jsx
+++ src/Components/Calculator/Calculator.jsx
@@ -3,6 +3,11 @@ import React from "react";
 function Calculator(){
     return(
         <div className="calculator-body">
+            <div className="calculator-body__output">
+                <div className="previous-operand"></div>
+                <div className="current-operand"></div>
+            </div>
+            <div className="calculator-body__line"></div>
 
         </div>
 
