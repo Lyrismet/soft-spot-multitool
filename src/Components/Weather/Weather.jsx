44f631aff95a7493944d47201fe4691d8b14176b
Lyrismet <ana0203tir@gmail.com>
2023-04-23 22:21:16 +0700

    Updating Weather module

--- src/Components/Weather/Weather.jsx
+++ src/Components/Weather/Weather.jsx
@@ -12,7 +12,7 @@ function Weather() {
 
     const search = evt => {
         if (evt.key === "Enter") {
-            fetch(`${api.base}weather?q=${query}&units=metric&APPID=&{api.key}`)
+            fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
                 .then(res => res.json())
                 .then(result => {
                     setQuery('');
