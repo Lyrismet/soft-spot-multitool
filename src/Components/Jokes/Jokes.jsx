7ebb0a01e9292548de06cdde0f9cac3592bbc6de
Lyrismet <ana0203tir@gmail.com>
2023-04-29 22:59:07 +0700

    Updated

--- src/Components/Jokes/Jokes.jsx
+++ src/Components/Jokes/Jokes.jsx
@@ -1,5 +1,15 @@
+import {useState} from "react";
 
+const api = {
+    url: "https://official-joke-api.appspot.com/random_joke"
+}
 function Jokes() {
+    const [randomJoke, setRandomJoke] = useState(null);
+    const generate = () => {
+        fetch(api.url).then(res => res.json()).then(result => {
+
+        })
+    }
     return (
         <div className="jokes-section">
             <h1 className="jokes-section__title">Generate random joke</h1>
@@ -7,7 +17,9 @@ function Jokes() {
                 <p className="jokes-section__setup">What is the tallest building in the world?</p>
                 <p className="jokes-section__punchline">The library, it’s got the most stories!</p>
             </div>
-            <button className="jokes-section__button">generate!</button>
+            <button className="jokes-section__button"
+            onClick={generate}
+            >generate!</button>
         </div>
     )
 }
